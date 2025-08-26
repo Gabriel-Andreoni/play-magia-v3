f9846c6c9ea63b37ac68c7e8f141b7ed62056f48
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2025-08-26 18:24:34 -0300

    Update manifest.ts

--- app/lib/manifest.ts
+++ app/lib/manifest.ts
@@ -1,5 +1,5 @@
 import Manifest from "@mnfst/sdk";
 
-const apiURL = process.env.API_URL || "http://localhost:1111";
+const apiURL = process.env.API_URL || "https://api-play-magia-production.up.railway.app";
 
 export const manifest = new Manifest(apiURL);
