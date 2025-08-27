ca7c60d790a120eaa290898bc3518590842af823
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2025-08-27 17:57:08 -0300

    Update manifest.ts

--- app/lib/manifest.ts
+++ app/lib/manifest.ts
@@ -1,5 +1,5 @@
 import Manifest from "@mnfst/sdk";
 
-const apiURL = `${process.env.API_URL}/api` || "http://localhost:1111";
+const apiURL = process.env.API_URL || "http://localhost:1111";
 
 export const manifest = new Manifest(apiURL);
