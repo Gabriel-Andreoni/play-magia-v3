7fd4297bf8931595d72a49e24b8f3aaa76ab79b9
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2025-09-18 17:05:54 -0300

    updates

--- app/playgrounds/page.tsx
+++ app/playgrounds/page.tsx
@@ -24,18 +24,18 @@ export default function Playgrounds() {
                     {/* Texto */}
                     <div className="w-1/2 flex flex-col">
                         <div className="w-[90%] p-4 rounded-[1.8em] bg-[#F5F4F2]">
-                            <h1 className="text-2xl font-semibold">Playgrounds</h1>
-                            <h3 className="mt-4 text-base">
+                            <h1 className="text-2xl font-semibold">Mais do que Playgrounds, Experiências</h1>
+                            <h3 className="mt-4 text-base font-light">
                                 Conheça Nosso Portfólio de Playgrounds de Madeira: Diversão, Segurança e Personalização. <br />
                                 Explore nossos modelos exclusivos ou crie junto com nossa equipe o playground dos seus sonhos.
                             </h3>
                         </div>
 
                         <div className="w-[90%] mt-8 p-4 bg-[#F5F4F2] rounded-[1.8em] flex flex-col">
-                            <p className="mt-4 leading-relaxed">
+                            <p className="mt-4 leading-relaxed font-light">
                                 Em nosso portfólio, você encontra uma seleção dos principais modelos de playgrounds de madeira, desenvolvidos para diferentes tipos de espaços e necessidades. Cada projeto é pensado para proporcionar diversão, estimular a criatividade e garantir a máxima segurança das crianças, utilizando madeira de qualidade, design inovador e acabamentos impecáveis. Apresentamos opções que vão desde estruturas clássicas até grandes projetos personalizados, para áreas residenciais, escolas, condomínios, hotéis, clubes e praças públicas.
                             </p>
-                            <p className="mt-4 leading-relaxed">
+                            <p className="mt-4 leading-relaxed font-light">
                                 Se deseja algo único e totalmente adaptado ao seu espaço, nossa equipe de engenharia está pronta para desenvolver soluções de playgrounds de madeira sob medida, aliando segurança, estética, funcionalidade e durabilidade. Conte com nosso atendimento especializado para tirar dúvidas, adaptar projetos ou criar novos conceitos exclusivos para seu ambiente. Navegue pelo nosso portfólio, inspire-se nos modelos e entre em contato para transformar seu projeto em realidade. A diversão começa aqui — e é feita do seu jeito!
                             </p>
                         </div>
@@ -53,7 +53,7 @@ export default function Playgrounds() {
             {/* PRODUTOS RELACIONADOS */}
             <div className="w-full my-24">
                 <ul className="w-[90%] mx-auto flex flex-wrap gap-16">
-                    <h2 className="w-full text-2xl font-semibold">Playgrounds Ideais para o Seu Espaço</h2>
+                    <h2 className="w-full text-2xl font-semibold">Playgrounds que Fazem a Diferença</h2>
 
                     <ProdutosRelacionados quantProdutos={17} />
                 </ul>
--- app/sections/Clients/index.tsx
+++ app/sections/Clients/index.tsx
@@ -4,8 +4,7 @@ export async function Clients() {
   return (
     <section className="w-full h-auto flex flex-col justify-center items-center">
       <div className="w-full p-[4em] flex flex-col items-center">
-        <h2 className="text-[6em] text-[#A05625] text-center my-[0.2em]">Nossos Clientes</h2>
-        <p className="text-center text-[1.2em] font-bold">Onde Nossos Playgrounds Fazem a Diferença.</p>
+        <h2 className="text-[6em] text-[#A05625] text-center my-[0.2em]">Onde Fazemos a Diferença</h2>
       </div>
 
       <div className="w-full h-full pb-[8em] flex justify-center items-center">
