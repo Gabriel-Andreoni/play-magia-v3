ca89738183d4a5b6939b5fa4555f5741ed2f9b3f
Gabriel-Andreoni <gabriel.andreoni@hotmail.com>
2025-12-04 19:26:27 -0300

    Criação do menu mobile

--- app/componentes/Menu/index.tsx
+++ app/componentes/Menu/index.tsx
@@ -1,15 +1,84 @@
+"use client";
+
 import Link from 'next/link';
 
 import Logo from './img/play-magia-logo.png';
+import MenuIcon from './img/menu.png';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export function Menu() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <header
     className={`w-full h-[110px] flex items-center sticky top-0 backdrop-blur-md shadow-lg  transition-[height] duration-300 shadow-slate-950/5 z-[1000]`}
     >
       <Image className="ml-6 rounded-full" width={100} height={100} src={Logo.src} alt="Logo da Play Magia" />
-      <nav className="w-full h-full">
+
+      <button className="lg:hidden ml-auto mr-6" onClick={toggleMobileMenu}>
+        <Image src={MenuIcon.src} alt="Menu" width={40} height={40} />
+      </button>
+
+      {/* Mobile Menu */}
+      {isMobileMenuOpen && (
+        <div className="lg:hidden absolute top-[110px] left-0 w-full bg-[#4D5020] shadow-lg py-4 transition-all duration-300 ease-in-out backdrop-blur-lg">
+          <ul className="flex flex-col items-center gap-4">
+            <li className="list-none text-[1.2em] font-bold cursor-pointer">
+              <Link
+                href="/"
+                className="text-white relative after:content-[''] after:absolute after:left-0 after:top-[110%] after:w-1/2 after:h-[2px] after:bg-[#4D5020] after:transition-all after:duration-300 hover:after:w-full"
+                onClick={toggleMobileMenu}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="list-none text-[1.2em] font-bold cursor-pointer">
+              <Link
+                href="/about"
+                className="text-white relative after:content-[''] after:absolute after:left-0 after:top-[110%] after:w-1/2 after:h-[2px] after:bg-[#4D5020] after:transition-all after:duration-300 hover:after:w-full"
+                onClick={toggleMobileMenu}
+              >
+                Sobre Nós
+              </Link>
+            </li>
+            <li className="list-none text-[1.2em] font-bold cursor-pointer">
+              <Link
+                href="/playgrounds"
+                className="text-white relative after:content-[''] after:absolute after:left-0 after:top-[110%] after:w-1/2 after:h-[2px] after:bg-[#4D5020] after:transition-all after:duration-300 hover:after:w-full"
+                onClick={toggleMobileMenu}
+              >
+                Playgrounds
+              </Link>
+            </li>
+            <li className="list-none text-[1.2em] font-bold cursor-pointer">
+              <Link
+                href="/orcamento"
+                className="text-white relative after:content-[''] after:absolute after:left-0 after:top-[110%] after:w-1/2 after:h-[2px] after:bg-[#4D5020] after:transition-all after:duration-300 hover:after:w-full"
+                onClick={toggleMobileMenu}
+              >
+                Faça um Orçamento
+              </Link>
+            </li>
+            <li className="list-none text-[1.2em] font-bold cursor-pointer">
+              <Link
+                href="#"
+                className="text-white relative after:content-[''] after:absolute after:left-0 after:top-[110%] after:w-1/2 after:h-[2px] after:bg-[#4D5020] after:transition-all after:duration-300 hover:after:w-full"
+                onClick={toggleMobileMenu}
+              >
+                Contato
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+
+      {/* Desktop Menu */}
+      <nav className="hidden lg:flex w-full h-full">
         <ul className="w-full h-full flex justify-center items-center gap-[4em]">
           <li className="list-none text-[1.2em] font-bold cursor-pointer">
             <Link
