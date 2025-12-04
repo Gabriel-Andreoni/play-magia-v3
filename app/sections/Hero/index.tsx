aef691bf6b684e0689fc3c0a1821c2d8ba474c2d
Gabriel-Andreoni <gabriel.andreoni@hotmail.com>
2025-12-04 19:35:57 -0300

    Criação da responsividade da seção Hero

--- app/sections/Hero/index.tsx
+++ app/sections/Hero/index.tsx
@@ -6,19 +6,19 @@ import { BotaoWhatsAppp } from '@/app/componentes/BotaoWhatsAppp';
 
 export function Hero() {
     return (
-        <section className="w-full h-screen p-16 flex flex-col overflow-hidden ">
+        <section className="w-full h-screen lg:p-16 sm:p-8 flex flex-col overflow-hidden ">
             <div className={`relative w-full h-[85%] flex flex-col justify-center items-center rounded-[4em] bg-cover bg-center shadow-2xl shadow-slate-950/50 z-[10] after:content-[''] after:w-full after:h-full after:absolute after:inset-0 after:bg-black/50 after:rounded-[4em] after:z-[-1] `} style={{ backgroundImage: `url(${HeroImage.src})` }}>
                 
-                <h1 className="w-full mb-[0.1em] lg:text-[6em] md:text-[4em] text-center text-white ">
+                <h1 className="w-full mb-[0.1em] lg:text-[6em] md:text-[4em] sm:text-[2em] text-center text-white ">
                     Crie <span className="text-[#FAE8BF]">Memórias Incríveis</span>
                 </h1>
 
-                <p className="w-[70%] text-[1.2em] text-center leading-[1.5em] text-white font-light">
+                <p className="w-[70%] lg:text-[1.2em] md:text-[1em] sm:text-[0.8em] text-center leading-[1.5em] text-white font-light">
                     Transforme o espaço da sua escola, condomínio ou parque em um lugar mágico.<br/> Com playgrounds seguros, duráveis e cheios de diversão, oferecemos experiências que encantam crianças e tranquilizam adultos. Conheça nossas soluções e crie memórias que vão durar para sempre.
                 </p>
 
-                <Link className="w-fit p-6 mt-8 rounded-full bg-[#4D5020]/95 text-white cursor-pointer text-[1.1em]" href="#produtos">
-                    <Image src={ArrowDown.src} alt="ícone de um seta para baixo" width={25} height={25} />
+                <Link className="w-fit lg:p-6 sm:p-4 mt-8 rounded-full bg-[#4D5020]/95 text-white cursor-pointer text-[1.1em]" href="#produtos">
+                    <Image className="" src={ArrowDown.src} alt="ícone de um seta para baixo" width={25} height={25} />
                 </Link>
                 <BotaoWhatsAppp />
             </div>
