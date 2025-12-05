ea7bd6849e2b4a2beb776685e8ecbc88256cd640
Gabriel-Andreoni <gabriel.andreoni@hotmail.com>
2025-12-05 07:53:28 -0300

    Criação da responsividade da seção Footer

--- app/sections/Footer/index.tsx
+++ app/sections/Footer/index.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export function Footer() {
   return (
-    <footer className="w-full h-auto bg-[#BC7823] p-[6em]">
-      <div className="w-full p-[2em] flex justify-between flex-wrap gap-6">
+    <footer className="w-full h-auto bg-[#BC7823] lg:p-[6em]">
+      <div className="w-full p-[2em] flex sm:flex-col justify-between flex-wrap gap-6">
         <div className="flex flex-col gap-4">
           <h4 className="font-bold text-lg">Nosso Endereço</h4>
           <p className="font-bold">Rua Doutor Eraldo Aurélio Franzese, 71 - Jardim Paiquerê - Valinhos - São Paulo</p>
@@ -36,11 +36,7 @@ export function Footer() {
         <iframe
           src="https://www.google.com/maps/embed?pb=!4v1761775894858!6m8!1m7!1sO9lIlAQQdYePxf-JI8LzRw!2m2!1d-22.98288023660557!2d-47.01303833351664!3f48.73499293564683!4f-17.800162325064633!5f0.7820865974627469"
           allowFullScreen
-          style={{
-            width: '400px',
-            border: 0,
-            borderRadius: '0.8em',
-          }}
+          className="lg:w-[400px] lg:h-[300px] sm:w-full sm:h-[200px]"
         ></iframe>
       </div>
     </footer>
