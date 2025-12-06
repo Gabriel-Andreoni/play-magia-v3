9495c9b246f845f03ce760d46f5b27ef99073c85
Gabriel-Andreoni <gabriel.andreoni@hotmail.com>
2025-12-06 13:13:11 -0300

    Alteração na responsividade da seção Hero

--- app/sections/Hero/index.tsx
+++ app/sections/Hero/index.tsx
@@ -5,13 +5,13 @@ import { BotaoWhatsAppp } from '@/app/componentes/BotaoWhatsAppp';
 export function Hero() {
     return (
         <section className="w-full h-screen lg:p-16 sm:p-8 flex flex-col overflow-hidden ">
-            <div className={`relative w-full h-[85%] flex flex-col justify-center items-center rounded-[4em] bg-cover bg-center shadow-2xl shadow-slate-950/50 z-[10] after:content-[''] after:w-full after:h-full after:absolute after:inset-0 after:bg-black/50 after:rounded-[4em] after:z-[-1] `} style={{ backgroundImage: `url(/images/hero-background.jpg)` }}>
+            <div className={`relative w-full h-[85%] flex flex-col lg:justify-center sm:justify-evenly items-center rounded-[4em] bg-cover bg-center shadow-2xl shadow-slate-950/50 z-[10] after:content-[''] after:w-full after:h-full after:absolute after:inset-0 after:bg-black/50 after:rounded-[4em] after:z-[-1] `} style={{ backgroundImage: `url(/images/hero-background.jpg)` }}>
                 
                 <h1 className="w-full mb-[0.1em] lg:text-[6em] md:text-[4em] sm:text-[2em] text-center text-white ">
                     Crie <span className="text-[#FAE8BF]">Memórias Incríveis</span>
                 </h1>
 
-                <p className="w-[70%] lg:text-[1.2em] md:text-[1em] sm:text-[0.8em] text-center leading-[1.5em] text-white font-light">
+                <p className="w-[70%] text-[1.2em] text-center leading-[1.5em] text-white font-light">
                     Transforme o espaço da sua escola, condomínio ou parque em um lugar mágico.<br/> Com playgrounds seguros, duráveis e cheios de diversão, oferecemos experiências que encantam crianças e tranquilizam adultos. Conheça nossas soluções e crie memórias que vão durar para sempre.
                 </p>
 
